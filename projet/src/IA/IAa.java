73c308f3cf81a126797427b8c5d68c64673db89b
Pomme-Pomme <lilian.barreteau@laposte.net>
2015-12-17 12:18:40 +0100

    Developpement d'une fonction primaire pour le déplacement chasseur

--- projet/src/IA/IAa.java
+++ projet/src/IA/IAa.java
@@ -1,3 +1,5 @@
+package project;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -30,34 +32,69 @@ public Case deplacementLePlusViable() {
 	ArrayList<Case> listCellAdja = getCelluleAdjacente();
 	ArrayList<Integer> listInt = new ArrayList<Integer>();
 	int compteur;
+	//Pour toutes les cases adjacentes à l'actuelle
 	for(Case c:listCellAdja) {
 		compteur = 0;
-		if(!c.getSafe()) {
+		
+		if(c.getVisite()){
+			compteur += 1.5;	//Si on l'a déjà visité, on évite d'y aller
+		}else{
 			if(c.getDangersPuit()) {
-				compteur++;
+				compteur++;		//Si il y a un dangers de puit
 			}
 			if (c.getDangersWumpus()) {
-				compteur++;
+				compteur++;		//Si il y a un dangers de wumpus
 			}
 			if(c.getWumpus()) {
+				compteur +=10;	//Si il y a wumpus, on n'y va pas
 			}
 			if (c.getPuit()) {
-				compteur += 10;
+				compteur += 10;	//Si il y a un puit, on y va pas
 			}
 		}
 		listInt.add(compteur);
 	}
+	//On cherche le plus petit nombre trouvé
 	int minimum = Collections.min(listInt);
 
+	//On cherche quelles cases ont reçu ce score
 	for(int i=0; i<listInt.size(); i++){
 		if(listInt.get(i) != minimum){
 			listCellAdja.remove(i);
 		}
 	}
 
+	//On retourne une des cases qui a eu le plus petit score
 	return listCellAdja.get((int)(Math.random()*listCellAdja.size()));
 }
 
+/**
+*Envoie la case vers laquelle le déplacement est le moins dangereux et qui se rapproche du wumpus
+*/
+public Case deplacementChasseur(ArrayList<Integer> posWumpus) {
+	
+	//Si la case en bas se rapproche
+	if(Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY)) >  
+		Math.abs(posWumpus.get(0)-(currentX-1)) + Math.abs(posWumpus.get(1)-(currentY))){
+		return labyrinth[currentX-1][currentY];
+	}
+	//Si la case en haut se rapproche
+	if(Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY)) >  
+		Math.abs(posWumpus.get(0)-(currentX+1)) + Math.abs(posWumpus.get(1)-(currentY))){
+		return labyrinth[currentX+1][currentY];
+	}
+	//Si la case à gauche se rapproche
+	if(Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY)) >  
+		Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY-1))){
+		return labyrinth[currentX][currentY-1];
+	}
+	//Si la case à droite se rapproche
+	if(Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY)) >  
+		Math.abs(posWumpus.get(0)-(currentX)) + Math.abs(posWumpus.get(1)-(currentY+1))){
+		return labyrinth[currentX-1][currentY+1];
+	}
+	return null;
+}
 
 /**
 *Envoie la liste des cases adjacentes
@@ -84,7 +121,6 @@ public ArrayList<Case> getCelluleAdjacente() {
 */
 public void miseAJour(ArrayList<String> message){
 
-	labyrinth[currentX][currentY].setSafe(true);
 	labyrinth[currentX][currentY].setVisite(true);
 
 	//Si on sent une odeur infame
@@ -139,7 +175,40 @@ public void miseAJour(ArrayList<String> message){
 
 			}
 		}
-	}	
+	}
+	
+	//On va regarder le nombre de dangersPuit et dangersWumpus
+	int compteurPuit=0;
+	int xPuit=-1;
+	int yPuit=-1;
+	int compteurWumpus=0;
+	int xWumpus=-1;
+	int yWumpus=-1;
+	
+	for (int i=0; i<boundX; i++) {
+		for (int j=0; j<boundY; j++) {
+			if(labyrinth[i][j].getDangersPuit()){
+				compteurPuit++;
+				xPuit=i;
+				yPuit=j;
+			}
+			if(labyrinth[i][j].getDangersWumpus()){
+				compteurWumpus++;
+				xWumpus=i;
+				yWumpus=j;
+			}
+		}
+	}
+	
+	//Si il n'y a qu'un seul dangers, alors le puit est à cette position
+	if(compteurPuit == 1){
+		labyrinth[xPuit][yPuit].setPuit(true);
+	}
+	
+	//Si il n'y a qu'un seul dangers, alors le puit est à cette position
+	if(compteurWumpus == 1){
+		labyrinth[xWumpus][yWumpus].setWumpus(true);
+	}
 
 }
 
@@ -150,36 +219,146 @@ public String jouer(int x, int y, ArrayList<String> message){
 
 	this.currentX=x;
 	this.currentY=y;
+	boolean wumpusTrouve=false;
+	Case wumpusAcote=null;
 
 	//Met à jour le plateau en fonction du message
 	this.miseAJour(message);
 
-	//Cherche la case avec le deplacement le plus viable
-	Case caseDirection = this.deplacementLePlusViable();
+	//On cherche si un wumpus a été trouvé
+	wumpusTrouve = this.presenceWumpus();
+	
+	if(wumpusTrouve){//Si un wumpus à été trouvé
+	
+		//On cherche si il y a un wumpus à cote
+		wumpusAcote = this.wumpusProche();
+		if(wumpusAcote != null){//Si un wumpus est à cote
+			if (currentY-1 >= 0) {
+				if(labyrinth[x][y-1] == wumpusAcote){	//Case de gauche
+					return "t o";
+				}
+			}
+			if (currentY+1 <= 4) {
+				if(labyrinth[x][y+1] == wumpusAcote){	//Case de droite
+					return "t e";
+				}
+			}
+			if (currentX-1 >= 0) {
+				if(labyrinth[x-1][y] == wumpusAcote){	//Case du bas
+					return "t s";
+				}
+			}
+			if (currentX+1 <= 4) {
+				if(labyrinth[x+1][y] == wumpusAcote){ //Case du haut
+					return "t n";
+				}
+			}
+			return "oups";
+		}else{//Si le wumpus n'est pas à cote
+			//On cherche la position du wumpus et on cherche un deplacement qui se rapproche de lui
+			ArrayList<Integer> posWumpus = positionWumpus(); 
+			Case caseDirection = this.deplacementChasseur(posWumpus);
+			if(caseDirection != null){
+				return messageAEnvoyer(caseDirection);
+			}else{
+				return "oups";
+			}
+		}
+		
+	
+	}else{//Si le wumpus n'a pas été trouvé	
+		//Cherche la case avec le deplacement le plus viable
+		Case caseDirection = this.deplacementLePlusViable();
+		
+		if(caseDirection != null){
+			return messageAEnvoyer(caseDirection);
+		}else{
+			return "oups";
+		}
+	}
+}
+
+/**
+ * Genere le message à partir de la case
+ */
+public String messageAEnvoyer(Case caseDirection){
 	
 	if (currentY-1 >= 0) {
-		if(labyrinth[x][y-1] == caseDirection){	//Case de gauche
+		if(labyrinth[currentX][currentY-1] == caseDirection){	//Case de gauche
 			return "d o";
 		}
 	}
 	if (currentY+1 <= 4) {
-		if(labyrinth[x][y+1] == caseDirection){	//Case de droite
+		if(labyrinth[currentX][currentY+1] == caseDirection){	//Case de droite
 			return "d e";
 		}
 	}
 	if (currentX-1 >= 0) {
-		if(labyrinth[x-1][y] == caseDirection){	//Case du bas
+		if(labyrinth[currentX-1][currentY] == caseDirection){	//Case du bas
 			return "d s";
 		}
 	}
 	if (currentX+1 <= 4) {
-		if(labyrinth[x+1][y] == caseDirection){ //Case du haut
+		if(labyrinth[currentX+1][currentY] == caseDirection){ //Case du haut
 			return "d n";
 		}
 	}
-	
 
 	return "Oups";
+	
+}
+
+
+/**
+ * Renvoie vrai si on a trouvé le wumpus, faux sinon
+ */
+public boolean presenceWumpus(){
+	
+	boolean test=false;
+	
+	for (int i=0; i<boundX; i++) {
+		for (int j=0; j<boundY; j++) {
+			if(labyrinth[i][j].getWumpus()){
+				test = true;
+			}
+		}
+	}
+	
+	return test;
+}
+
+/**
+ * Renvoie la position du wumpus
+ */
+public ArrayList<Integer> positionWumpus(){
+	
+	ArrayList<Integer> listPosition = new ArrayList<Integer>();
+	
+	for (int i=0; i<boundX; i++) {
+		for (int j=0; j<boundY; j++) {
+			if(labyrinth[i][j].getWumpus()){
+				listPosition.add(i);
+				listPosition.add(j);
+			}
+		}
+	}
+
+	return listPosition;
+}
+
+/**
+ * Renvoie la case où est présent le wumpus, null si il n'est pas à cote 
+ */
+public Case wumpusProche(){
+	
+	ArrayList<Case> listCellAdja = getCelluleAdjacente();
+	
+	for(Case c : listCellAdja){
+		if(c.getWumpus()){
+			return c;
+		}
+	}
+	return null;
 }
 
 // ---------------------------------------------------------------------- //
@@ -201,7 +380,7 @@ public String jouer(int x, int y, ArrayList<String> message){
 		}
 
 /**
- *Affiche le tableau		
+ *Fonction de test pour afficher les dangers de Puit		
  */
 		public void afficheLabPuit(){
 			for (int i=0; i<boundX; i++) {
@@ -211,7 +390,10 @@ public String jouer(int x, int y, ArrayList<String> message){
 				}
 			}
 		}
-		
+
+/**
+*Fonction de test pour afficher les dangers de Wumpus		
+*/
 		public void afficheLabWumpus(){
 			for (int i=0; i<boundX; i++) {
 				System.out.print("\n");
@@ -220,3 +402,5 @@ public String jouer(int x, int y, ArrayList<String> message){
 				}
 			}
 		}
+		
+}
