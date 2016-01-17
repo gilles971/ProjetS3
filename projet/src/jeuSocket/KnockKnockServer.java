440d58bc7f96ef9e396b552241feb734d8b71036
Rintarou <merlin.barzilai@gmail.com>
2016-01-17 11:31:40 +0100

    Les Sockets ça marche avec le jeu c'est trop de la balle et c'est cool

--- projet/src/jeuSocket/KnockKnockServer.java
+++ projet/src/jeuSocket/KnockKnockServer.java
@@ -57,7 +57,7 @@ public class KnockKnockServer {
             
             // Initiate conversation with client
             KnockKnockProtocol kkp = new KnockKnockProtocol();
-            outputLine = kkp.processInput(null);
+            outputLine = "welcome to hunt the wumpus";//kkp.processInput(null);
             out.println(outputLine);
 
             while ((inputLine = in.readLine()) != null) {
